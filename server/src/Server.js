efcd0b1c1dd7867726856ce2748034de23be1327
Kelly <kellyaccnts@gmail.com>
2020-02-15 23:43:34 -0700

    chat server

--- server/src/Server.js
+++ server/src/Server.js
@@ -1,10 +1,31 @@
-const app = require('express')();
-const http = require('http').createServer(app);
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const socketio = require("socket.io");
 
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirectoryPath));
+
+io.on("connection", socket => {
+    console.log("new user connected");
+    socket.emit("onUserJoined", "Welcome!");
+    socket.broadcast.emit("sendMessage", "A new user has joined the chat.");
+
+    socket.on("onSendMessage", (msg, callback) => {
+        io.emit("sendMessage", msg);
+        callback();
+    });
+    socket.on("disconnect", () => {
+        io.emit("sendMessage", "A user has left the chat.");
+    });
 });
 
-http.listen(3001, function(){
-  console.log('Running on port 3001');
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
