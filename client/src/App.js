83d3000bf0abf28a91776cd70be3b04613172025
Kelly <kellyaccnts@gmail.com>
2020-02-16 17:08:31 -0700

    client form & socket base functionality

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,36 @@ import socketIOClient from "socket.io-client";
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.port = process.env.port || 3000;
+    this.io = socketIOClient(`localhost:${this.port}`);
+    this.state = {
+      value: ""
+    };
   }
   componentDidMount() {
-    const socket = socketIOClient("localhost:3000");
-    socket.on("onUserJoined", msg => {
+    this.io.on("onUserJoined", msg => {
       console.log(msg);
     });
 
-    socket.on("sendMessage", msg => {
+    this.io.on("sendMessage", msg => {
       console.log(msg);
     });
   }
+  componentWillUnmount() {
+    this.io.close();
+  }
+
   clickSendMessage = e => {
     e.preventDefault();
+
+    if (!this.state.value) return;
+    this.setState({ value: "" });
+    this.io.emit("onSendMessage", this.state.value, error => {
+      if (error) console.log(error);
+    });
+  };
+  handleChange = e => {
+    this.setState({ value: e.target.value });
   };
 
   render() {
@@ -31,7 +47,11 @@ class App extends Component {
         <main>
           <ul id="messages"></ul>
           <form action="">
-            <input id="m" />
+            <input
+              id="message-input"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
             <button onClick={this.clickSendMessage}>Send</button>
           </form>
         </main>
